d00f9a5619818a0ddfc4ceb88e0ffda651ffca78
mousterian <rivladi@gmail.com>
2014-07-21 13:54:03 -0700

    updated applciation js and changed the name of application css to application css scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require bootstrap
+//= require bootstrap/modal
+//= require bootstrap/dropdown
 //= require turbolinks
 //= require_tree .
 
