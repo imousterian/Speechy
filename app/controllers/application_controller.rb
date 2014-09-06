6241d398510fefe5173a6311f25311f65e5fe287
mousterian <rivladi@gmail.com>
2014-09-05 19:36:18 -0700

    removed skip before filter user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
     before_action :authenticate_user!, except: [:create_guest_user]
-    skip_before_filter :authenticate_user!
+
+    # skip_before_filter :authenticate_user! #??
 
     # skip_before_filter :verify_authenticity_token, only: [:create_guest_user]
 
