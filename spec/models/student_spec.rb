009534b797a60ee7634804b22134b10a913177ae
mousterian <rivladi@gmail.com>
2015-02-17 14:56:05 -0800

    deleted minitest dir and updated student_spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Student, :type => :model do
 
     let(:student) {FactoryGirl.create(:student)}
-    let(:student_response) {FactoryGirl.create(:student_response)}
+    # let(:student_response) {FactoryGirl.create(:student_response)}
 
     it "is valid when a name is present" do
         expect(student).to be_valid
@@ -14,7 +14,20 @@ RSpec.describe Student, :type => :model do
         student.valid?
         expect(student.errors[:name]).to match_array (["can't be blank"])
     end
-    it "correctly returns a percentage of answers"
-    it "correctly returns responses sorted by asc date"
+
+    it "correctly returns a percentage of answers" do
+        student = Student.create(name: 'Student')
+        student.student_responses.build(:emotion => 'sad', :created_at => 1.week.ago, :correct => true)
+        student.student_responses.build(:emotion => 'happy', :created_at => 2.weeks.ago, :correct => false)
+        student.save!
+
+        expect(student.percentages).to eq [
+            {:unit=>Date.parse(2.weeks.ago.to_s), :status=>"Correct", :val=>0},
+            {:unit=>Date.parse(2.weeks.ago.to_s), :status=>"Not correct", :val=>100},
+            {:unit=>Date.parse(1.week.ago.to_s), :status=>"Correct", :val=>100},
+            {:unit=>Date.parse(1.week.ago.to_s), :status=>"Not correct", :val=>0}]
+    end
+
     it "correctly converts response data to csv"
+    it "correctly returns a percentage of answers"
 end
